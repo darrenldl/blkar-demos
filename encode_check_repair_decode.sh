d190dddc9f0fddbeac1adcb76b8e8683e7b524ce
Darren Ldl <darrenldldev@gmail.com>
2019-04-12 22:19:01 +1000

    Added more dialogues

--- encode_check_repair_decode.sh
+++ encode_check_repair_decode.sh
@@ -8,15 +8,23 @@ truncate -s 10M test
 
 erun 'ls -l'
 
+erun '# we first encode the file'
+
 erun 'blkar encode test'
 
 erun 'ls -l'
 
+erun '# we take a look the container metadata to make sure we used the right file etc'
+
 erun 'blkar show test.ecsbx'
+
+erun '# we check the container blocks'
 
 erun 'blkar check test.ecsbx'
 
-erun '# we corrupt the archive randomly'
+erun '# everything looks fine'
+
+erun '# now we corrupt the archive randomly'
 
 erun 'dd if=/dev/urandom of=test.ecsbx bs=123 seek=$((RANDOM % 100)) count=30 conv=notrunc'
 
@@ -31,5 +39,7 @@ erun '# repairs were successful, so we decode the archive'
 erun 'blkar decode test.ecsbx test2'
 
 erun 'cmp test test2'
+
+erun '# everything checks out!'
 
 clean

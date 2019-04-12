f57a7f0e43238b543193b314624fd09ee8c4e2a1
Darren Ldl <darrenldldev@gmail.com>
2019-04-12 21:58:40 +1000

    Fixed asciinema record script

---
+++ record_via_asciinema.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $1 == "" ]]; then
+  echo "Please specify a demo to run"
+  exit 1
+fi
+
+demo="$1".sh
+cast_filename=$1.cast
+
+if [ ! -f "$demo" ]; then
+  echo "Demo \"$demo\" does not exist"
+  exit 1
+fi
+
+asciinema rec $cast_filename --overwrite -c ./$demo

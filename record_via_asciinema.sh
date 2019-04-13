74d27d60e0229ba5b78bb3d6212de05cda9f43bd
Darren Ldl <darrenldldev@gmail.com>
2019-04-13 14:42:31 +1000

    Added auto title grabbing

--- record_via_asciinema.sh
+++ record_via_asciinema.sh
@@ -8,9 +8,11 @@ fi
 demo=$1
 cast_filename=$1.cast
 
+title=$(grep "title" $demo | cut -d ' ' -f 4-)
+
 if [ ! -f "$demo" ]; then
   echo "Demo \"$demo\" does not exist"
   exit 1
 fi
 
-asciinema rec $cast_filename --overwrite -c ./$demo
+asciinema rec -t "$title" $cast_filename --overwrite -c ./$demo

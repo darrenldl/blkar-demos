cd6b03c8ea3302cb0b6f631d0a1d81beb9dbae77
Darren Ldl <darrenldldev@gmail.com>
2019-04-13 20:08:49 +1000

    Added adjust_burst.sh

---
+++ Container-sorting-scenarios-and-demos/adjust_burst.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# title : Blkar demo - sort mode, adjust burst error resistance level
+
+source setup.sh
+
+set_up
+
+make_blank_file_MB test 10
+
+blkar encode test &>/dev/null
+
+erun 'ls -l'
+
+erun 'blkar show --guess-burst --pv 0 test.ecsbx 2>&1 | grep "Best guess"'
+
+erun '# we see the current burst error resistance level is 10, and we want to adjust it to 20'
+
+erun 'blkar sort test.ecsbx test.ecsbx.new --burst 20'
+
+erun 'blkar show --guess-burst --pv 0 test.ecsbx.new 2>&1 | grep "Best guess"'
+
+clean

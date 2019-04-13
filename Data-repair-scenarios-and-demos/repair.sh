aeb5ea5452927c0dc5cf828e29e33f99fb3769e5
Darren Ldl <darrenldldev@gmail.com>
2019-04-13 22:41:08 +1000

    Added repair.sh

---
+++ Data-repair-scenarios-and-demos/repair.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# title : Blkar demo - decode multi-pass
+
+source setup.sh
+
+set_up
+
+make_random_file_MB test 1
+
+blkar encode test &>/dev/null
+
+rm test
+
+erun 'ls -l'
+
+erun '# we corrupt the archive first, then try to repair'
+
+erun 'dd if=/dev/urandom of=test.ecsbx bs=123 seek=$((RANDOM % 100)) count=30 conv=notrunc'
+
+erun 'blkar check -v test.ecsbx'
+
+erun_y 'blkar repair -v test.ecsbx'
+
+erun 'blkar check -v test.ecsbx'
+
+clean

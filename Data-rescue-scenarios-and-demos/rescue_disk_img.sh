d3f6f30c8babf2a87cf0d5bdc706057f95552e7f
Darren Ldl <darrenldldev@gmail.com>
2019-04-14 01:11:18 +1000

    Added rescue_disk_img.sh

---
+++ Data-rescue-scenarios-and-demos/rescue_disk_img.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# title : Blkar demo - rescue from disk image
+
+source setup.sh
+
+set_up
+
+make_random_file_MB test0 1
+make_random_file_MB test1 1
+make_blank_file_MB disk.img 100
+make_blank_file_MB blank 100
+
+blkar encode test0 &>/dev/null
+blkar encode test1 &>/dev/null
+
+cat test0.ecsbx >> disk.img
+cat blank >> disk.img
+cat test1.ecsbx >> disk.img
+
+rm test0
+rm test1
+rm blank
+
+rm test0.ecsbx
+rm test1.ecsbx
+
+erun 'ls -l'
+
+erun '# we make a folder to store the rescued data'
+
+erun 'mkdir rescued_data'
+
+erun '# we now launch the rescue'
+
+erun '# we also supply a log file location in case we need to interrupt blkar'
+
+erun 'blkar rescue disk.img rescued_data rescue.log'
+
+erun 'ls -l rescued_data'
+
+erun '# we can see the rescued containers are stored using their UIDs as names'
+
+erun '# we now check the metadata just to see what we rescued'
+
+for f in rescued_data/*; do
+    erun "blkar show $f"
+done
+
+clean
